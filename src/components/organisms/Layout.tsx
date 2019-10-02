0d12fb7b709a8814d02d6749e3c9f7ec7e1ebf38
Asle Berge <asle.berge@gmail.com>
2019-10-02 16:13:47 +0200

    resolve conflict

--- src/components/organisms/Layout.tsx
+++ src/components/organisms/Layout.tsx
@@ -4,10 +4,23 @@ import styled from 'styled-components'
 
 const LayoutContainer = styled.div`
   width: 100vw;
-  height: 100vh;
-  background-image: linear-gradient(to right bottom, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1);
-  /* background-image: linear-gradient(to right, #000000, #35161c, #652229, #97312e, #c7472a, #db5324, #ee6119, #ff7000, #ff7000, #ff7000, #ff7000, #ff7000); BOUVET FARGER */
-`
+  min-height: 100vh;
+  background-image: radial-gradient(
+    circle,
+    #574833,
+    #5a452a,
+    #5e4121,
+    #613d18,
+    #65380f,
+    #723b0b,
+    #7f3e07,
+    #8d4003,
+    #a84c02,
+    #c45801,
+    #e16401,
+    #ff7000
+  );
+`;
 
 interface Props {
   children: JSX.Element[] | JSX.Element;
