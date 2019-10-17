665ae7ef4f59b01495584be92d9343ca929a3816
Kristian Nesse <kristian.nesse@bouvet.no>
2019-10-17 13:45:20 +0200

    made an ooopsie

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
 //export const BASE_URL = 'https://rom-18-server.azurewebsites.net';
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'https://rom-18-server.azurewebsites.net';
 export const PIXABAY_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://pixabay.com/api';
 export const XKCD_URL = `${BASE_URL}/xkcd`;
 export const QUOTES_API = 'https://cors-anywhere.herokuapp.com/http://programming-quotes-api.herokuapp.com';
